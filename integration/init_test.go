71d41b2adae451270aa233056e56aa2773e3844f
Ryan Moran <rmoran@vmware.com>
2020-07-09 15:47:42 -0400

    Fixes packaging and integration tests

--- integration/init_test.go
+++ integration/init_test.go
@@ -1,47 +1,71 @@
 package integration_test
 
 import (
-	"bytes"
-	"fmt"
+	"encoding/json"
+	"os"
 	"path/filepath"
-	"strings"
 	"testing"
 	"time"
 
-	"github.com/cloudfoundry/dagger"
-	"github.com/paketo-buildpacks/packit/pexec"
+	"github.com/paketo-buildpacks/occam"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
 
 	. "github.com/onsi/gomega"
 )
 
-var (
-	rackupBuildpack        string
-	mriBuildpack           string
-	bundlerBuildpack       string
-	bundleInstallBuildpack string
-)
+var settings struct {
+	Buildpacks struct {
+		Rackup struct {
+			Online string
+		}
+		MRI struct {
+			Online string
+		}
+		Bundler struct {
+			Online string
+		}
+		BundleInstall struct {
+			Online string
+		}
+	}
+
+	Config struct {
+		MRI           string `json:"mri"`
+		Bundler       string `json:"bundler"`
+		BundleInstall string `json:"bundle-install"`
+	}
+}
 
 func TestIntegration(t *testing.T) {
 	Expect := NewWithT(t).Expect
 
+	file, err := os.Open("../integration.json")
+	Expect(err).NotTo(HaveOccurred())
+
+	Expect(json.NewDecoder(file).Decode(&settings.Config)).To(Succeed())
+	Expect(file.Close()).To(Succeed())
+
 	root, err := filepath.Abs("./..")
 	Expect(err).NotTo(HaveOccurred())
 
-	rackupBuildpack, err = dagger.PackageBuildpack(root)
+	buildpackStore := occam.NewBuildpackStore()
+
+	settings.Buildpacks.Rackup.Online, err = buildpackStore.Get.
+		WithVersion("1.2.3").
+		Execute(root)
 	Expect(err).NotTo(HaveOccurred())
 
-	// HACK: we need to fix dagger and the package.sh scripts so that this isn't required
-	rackupBuildpack = fmt.Sprintf("%s.tgz", rackupBuildpack)
-
-	mriBuildpack, err = dagger.GetLatestCommunityBuildpack("paketo-community", "mri")
+	settings.Buildpacks.MRI.Online, err = buildpackStore.Get.
+		Execute(settings.Config.MRI)
 	Expect(err).NotTo(HaveOccurred())
 
-	bundlerBuildpack, err = dagger.GetLatestCommunityBuildpack("paketo-community", "bundler")
+	settings.Buildpacks.Bundler.Online, err = buildpackStore.Get.
+		Execute(settings.Config.Bundler)
 	Expect(err).NotTo(HaveOccurred())
 
-	bundleInstallBuildpack, err = dagger.GetLatestCommunityBuildpack("paketo-community", "bundle-install")
+	settings.Buildpacks.BundleInstall.Online, err = buildpackStore.Get.
+		Execute(settings.Config.BundleInstall)
 	Expect(err).NotTo(HaveOccurred())
 
 	SetDefaultEventuallyTimeout(10 * time.Second)
@@ -50,44 +74,5 @@ func TestIntegration(t *testing.T) {
 	suite("SimpleApp", testSimpleApp)
 	suite("SinatraApp", testSinatraApp)
 
-	defer AfterSuite(t)
 	suite.Run(t)
-}
-
-func AfterSuite(t *testing.T) {
-	var Expect = NewWithT(t).Expect
-
-	Expect(dagger.DeleteBuildpack(rackupBuildpack)).To(Succeed())
-	Expect(dagger.DeleteBuildpack(mriBuildpack)).To(Succeed())
-	Expect(dagger.DeleteBuildpack(bundlerBuildpack)).To(Succeed())
-	Expect(dagger.DeleteBuildpack(bundleInstallBuildpack)).To(Succeed())
-}
-
-func GetGitVersion() (string, error) {
-	gitExec := pexec.NewExecutable("git")
-	revListOut := bytes.NewBuffer(nil)
-
-	err := gitExec.Execute(pexec.Execution{
-		Args:   []string{"rev-list", "--tags", "--max-count=1"},
-		Stdout: revListOut,
-	})
-
-	if revListOut.String() == "" {
-		return "0.0.0", nil
-	}
-
-	if err != nil {
-		return "", err
-	}
-
-	stdout := bytes.NewBuffer(nil)
-	err = gitExec.Execute(pexec.Execution{
-		Args:   []string{"describe", "--tags", strings.TrimSpace(revListOut.String())},
-		Stdout: stdout,
-	})
-	if err != nil {
-		return "", err
-	}
-
-	return strings.TrimSpace(strings.TrimPrefix(stdout.String(), "v")), nil
 }
--- integration/sinatra_app_test.go
+++ integration/sinatra_app_test.go
@@ -58,7 +58,12 @@ func testSinatraApp(t *testing.T, context spec.G, it spec.S) {
 
 			var logs fmt.Stringer
 			image, logs, err = pack.WithNoColor().Build.
-				WithBuildpacks(mriBuildpack, bundlerBuildpack, bundleInstallBuildpack, rackupBuildpack).
+				WithBuildpacks(
+					settings.Buildpacks.MRI.Online,
+					settings.Buildpacks.Bundler.Online,
+					settings.Buildpacks.BundleInstall.Online,
+					settings.Buildpacks.Rackup.Online,
+				).
 				WithNoPull().
 				Execute(name, source)
 			Expect(err).NotTo(HaveOccurred(), logs.String())
@@ -81,11 +86,8 @@ func testSinatraApp(t *testing.T, context spec.G, it spec.S) {
 			Expect(err).NotTo(HaveOccurred())
 			Expect(string(content)).To(ContainSubstring("Hello world!"))
 
-			buildpackVersion, err := GetGitVersion()
-			Expect(err).NotTo(HaveOccurred())
-
 			Expect(logs).To(ContainLines(
-				fmt.Sprintf("Rackup Buildpack %s", buildpackVersion),
+				"Rackup Buildpack 1.2.3",
 				"  Writing start command",
 				`    bundle exec rackup -p "${PORT:-9292}"`,
 			))
