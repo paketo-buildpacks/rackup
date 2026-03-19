f5d963a927a8f2cba68719387d3fa438361ff68f
BG <bguttmann@anynines.com>
2026-03-19 07:30:44 +0100

    Switch to multi arch/ os; drop bionic

--- scripts/build.sh
+++ scripts/build.sh
@@ -73,6 +73,7 @@ function run::build() {
         arch=$(echo "${target}" | cut -d'/' -f2)
 
         util::print::title "Building run... for platform: ${platform} and arch: ${arch}"
+          mkdir -p "${platform}/${arch}/bin"
 
         GOOS=$platform \
         GOARCH=$arch \
@@ -116,6 +117,7 @@ function cmd::build() {
 
         if [[ -f "${src}/main.go" ]]; then
           util::print::title "Building ${name}... for platform: ${platform} and arch: ${arch}"
+            mkdir -p "${BUILDPACKDIR}/${platform}/${arch}/bin"
 
           GOOS=$platform \
           GOARCH=$arch \
